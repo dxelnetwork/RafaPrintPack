de10b7e73ccbed7b28a84585084cb774389f9028
DXEL Network <ask@dxel.net>
2024-06-25 23:46:55 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,65 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // AOS ANIMATIONS
+    AOS.init();
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    // NEWS IMAGE RESIZE
+    function NewsImageResize(){
+      $(".navbar").scrollspy({ offset: -76 });
+      
+      var LargeImage = $('.large-news-image').height();
+
+      var MinusHeight = LargeImage - 6;
+
+      $('.news-two-column').css({'height' : (MinusHeight - LargeImage / 2) + 'px'});
+    }
+
+    $(window).on("resize", NewsImageResize);
+    $(document).on("ready", NewsImageResize);
+
+    $('a[href*="#"]').click(function (event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top - 66
+          }, 1000);
+        }
+      }
+    });
+    
+  })(window.jQuery);
+
+
+
+  // Gallery.................
+  let slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
